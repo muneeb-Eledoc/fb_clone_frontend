7a1e63dc6ce32546a4af33c8801c0c22481f5d17
flask <muneebjs.css@gmail.com>
2023-03-24 10:51:07 +0500

    first commit

---
+++ src/axios.js
@@ -0,0 +1,6 @@
+import axios from "axios"
+const instance = axios.create({
+    baseURL: 'https://fd-clone-backend.vercel.app/api'
+});
+
+export default instance
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getStorage } from "firebase/storage";
+import { getMessaging } from "firebase/messaging";
+
+const firebaseConfig = {
+//   your config
+};
+
+const app = initializeApp(firebaseConfig)
+
+const storage = getStorage(app);
+
+const messaging = getMessaging(app);
+
+export { storage, messaging }
